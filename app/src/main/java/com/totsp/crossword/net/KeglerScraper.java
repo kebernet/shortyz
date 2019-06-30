85d4a7fb97a6ad022e90c88a17213d093693e291
Michael K Johnson <a1237@danlj.org>
2019-06-29 20:30:39 -0400

    Add new location for Kegler's Kryptics

--- app/src/main/java/com/totsp/crossword/net/KeglerScraper.java
+++ app/src/main/java/com/totsp/crossword/net/KeglerScraper.java
@@ -1,9 +1,11 @@
 package com.totsp.crossword.net;
 
 
-//http://www.lafn.org/~keglerron/Block_style/index.html
+// http://www.lafn.org/~keglerron/Block_style/index.html
+// replaced by https://kegler.gitlab.io/Block_style/
+// because lafn.org is gone
 public class KeglerScraper extends AbstractPageScraper {
     public KeglerScraper() {
-        super("http://www.lafn.org/~keglerron/Block_style/index.html", "Kegler's Kryptics");
+        super("https://kegler.gitlab.io/Block_style/", "Kegler's Kryptics");
     }
 }
